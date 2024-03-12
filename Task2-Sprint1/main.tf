91ade504540604a072eed2af70198035ea506b3b
Rayudu <Rayudu@gmail.com>
2024-03-12 12:15:59 +0530

    change the user_data in main.tf & create terraform.tf

--- Task2-Sprint1/main.tf
+++ Task2-Sprint1/main.tf
@@ -79,20 +79,21 @@ resource "aws_instance" "web-1" {
   subnet_id                   = aws_subnet.subnet1-public.id
   vpc_security_group_ids      = [aws_security_group.allow_all.id]
   associate_public_ip_address = true
+  user_data = file("terraform.sh")
   tags = {
     Name = "Server-1"
   }
-  user_data = <<-EOF
-		    #!/bin/bash
-        sudo apt-get update
-		    sudo apt-get install -y unzip
-        sudo apt-get install -y openjdk-11-jdk
-		    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-        https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-        echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-        https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-        /etc/apt/sources.list.d/jenkins.list > /dev/null
-        sudo apt-get update
-        sudo apt-get install jenkins -y
-  	EOF
+  # user_data = <<-EOF
+	# 	    #!/bin/bash
+  #       sudo apt-get update
+	# 	    sudo apt-get install -y unzip
+  #       sudo apt-get install -y openjdk-11-jdk
+	# 	    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  #       https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  #       echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  #       https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  #       /etc/apt/sources.list.d/jenkins.list > /dev/null
+  #       sudo apt-get update
+  #       sudo apt-get install jenkins -y
+  # 	EOF
 }
